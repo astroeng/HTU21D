83498333927e6dfac126f180e4047a6324575bc7
astroeng <dschacht@gmail.com>
2015-07-15 22:59:31 -0400

    Updated this library to not use a blocking method for the communication with the i2c device. Instead a state machine handles the 4 different interactions with the i2c device. This allows other things to be happening on the microcontroller while the 100+ms communication schedule is performed.

--- htu21d.cpp
+++ htu21d.cpp
@@ -21,6 +21,12 @@ HTU21D::HTU21D(Software_I2C* i2c_bus)
 {
   
   _i2c_bus = i2c_bus;
+  rawTemperature = 0;
+  rawHumidity = 0;
+  temperatureTime = 0;
+  humidityTime = 0;
+  requestTime = 0;
+  readState = RequestTemperature;
 
 }
 
@@ -28,35 +34,118 @@ HTU21D::HTU21D(Software_I2C* i2c_bus)
 void HTU21D::readData()
 {
   char error;
-  
   unsigned char data[3];
 
-  _i2c_bus->start_i2c();
-  _i2c_bus->write(HTU21D_ADDRESS);
-  _i2c_bus->write(START_TEMPERATURE_AND_HOLD);
-  _i2c_bus->stop_i2c();
-  delay(55);
-  _i2c_bus->start_i2c();
-  _i2c_bus->write(HTU21D_ADDRESS | 0x01);
-  data[0] = _i2c_bus->read(&error);
-  data[1] = _i2c_bus->read(&error);
-  data[2] = _i2c_bus->read(&error);
   
-  rawTemperature = ((int)data[0] << 8) | data[1];
+  /* This is the switch block that implements the functional activities for each
+     of the states.
+   */
 
-  _i2c_bus->start_i2c();
-  _i2c_bus->write(HTU21D_ADDRESS);
-  _i2c_bus->write(START_HUMIDITY_AND_HOLD);
-  _i2c_bus->stop_i2c();
-  delay(55);
-  _i2c_bus->start_i2c();
-  _i2c_bus->write(HTU21D_ADDRESS | 0x01);
-  data[0] = _i2c_bus->read(&error);
-  data[1] = _i2c_bus->read(&error);
-  data[2] = _i2c_bus->read(&error);
+  switch (readState)
+  {
+    /* In the request temperature state the command to start the temperature conversion
+       is sent to the device. The time that the request was sent is saved for use in the
+       state flow logic in the next switch block.
+     */
+    case RequestTemperature:
+      _i2c_bus->start_i2c();
+      _i2c_bus->write(HTU21D_ADDRESS);
+      _i2c_bus->write(START_TEMPERATURE_NOHOLD);
+      _i2c_bus->stop_i2c();
+      
+      requestTime = millis();
+      
+      break;
 
+    /* In the retrieve temperature state the i2c bus is used to read back the value
+       that was triggered in the previous state. This value consists of 3 bytes (high,
+       low, crc). For the moment the crc is ignored.
+       TODO: Implement crc functionality.
+     */
+    case RetrieveTemperature:
+      _i2c_bus->start_i2c();
+      _i2c_bus->write(HTU21D_ADDRESS | 0x01);
+      
+      temperatureTime = millis();
+      
+      data[0] = _i2c_bus->read(&error);
+      data[1] = _i2c_bus->read(&error);
+      data[2] = _i2c_bus->read(&error);
+      _i2c_bus->stop_i2c();
+      
+      rawTemperature = ((int)data[0] << 8) | data[1];
+      
+      break;
+      
+    /* The request humidity state is pretty much the same as the request temperature 
+       state excepth the command for humidity is sent.
+     */
+    case RequestHumidity:
+
+      _i2c_bus->start_i2c();
+      _i2c_bus->write(HTU21D_ADDRESS);
+      _i2c_bus->write(START_HUMIDITY_NOHOLD);
+      _i2c_bus->stop_i2c();
+      
+      requestTime = millis();
+      
+      break;
+    
+    /* The retrieve humidity state is pretty much the same as the retrieve temperature
+       state except the bytes from the humidity command are returned.
+       TODO: Implement CRC functions.
+     */
+    case RetrieveHumidity:
+      
+      _i2c_bus->start_i2c();
+      _i2c_bus->write(HTU21D_ADDRESS | 0x01);
+      
+      humidityTime = millis();
+      
+      data[0] = _i2c_bus->read(&error);
+      data[1] = _i2c_bus->read(&error);
+      data[2] = _i2c_bus->read(&error);
+      _i2c_bus->stop_i2c();
+      
+      rawHumidity = ((int)data[0] << 8) | data[1];
+      
+      break;
+    default:
+      /* do nothing */
+      /* WaitTemperature */
+      /* WaitHumidity */
+      break;
+  }
   
-  rawHumidity = ((int)data[0] << 8) | data[1];
+  /* This is the state flow logic. It must be below the state activity logic.
+   */
+  
+  switch (readState) 
+  {
+    case RequestTemperature:
+      readState = WaitTemperature;
+      break;
+    case WaitTemperature:
+      if (requestTime + SAMPLE_TIME < millis())
+      {
+        readState = RetrieveTemperature;
+      }
+      break;
+    case RetrieveTemperature:
+      readState = RequestHumidity;
+      break;
+    case RequestHumidity:
+      readState = WaitHumidity;
+    case WaitHumidity:
+      if (requestTime + SAMPLE_TIME < millis())
+      {
+        readState = RetrieveHumidity;
+      }
+      break;
+    case RetrieveHumidity:
+      readState = RequestTemperature;
+      break;
+  }
 
 }
 
@@ -70,6 +159,13 @@ int HTU21D::getTemperature()
 
 }
 
+int HTU21D::getTemperatureAge()
+{
+  
+  return millis() - temperatureTime;
+  
+}
+
 
 int HTU21D::getHumidity()
 {
@@ -79,4 +175,11 @@ int HTU21D::getHumidity()
   
   return value * 10.0;
   
-}
+}  
+
+int HTU21D::getHumidityAge()
+{
+  
+  return millis() - humidityTime;
+  
+}  
--- htu21d.h
+++ htu21d.h
@@ -5,16 +5,38 @@
 
 #include <Software_I2C.h>
 
+#define SAMPLE_TIME 55 /* ms */
+#define HOLD_TIME 10 /* ms */
+
+typedef enum
+{
+  RequestTemperature,
+  WaitTemperature,
+  RetrieveTemperature,
+  RequestHumidity,
+  WaitHumidity,
+  RetrieveHumidity
+} HTU21DReadStateType;
+
 class HTU21D
 {
 public:
   HTU21D(Software_I2C* i2c_bus);
   void readData();
   int getTemperature();
+  int getTemperatureAge();
   int getHumidity();
+  int getHumidityAge();
 private:
   unsigned int rawTemperature;
   unsigned int rawHumidity;
+  
+  unsigned long temperatureTime;
+  unsigned long humidityTime;
+  
+  unsigned long requestTime;
+
+  char readState;
   Software_I2C* _i2c_bus;
 
 };
